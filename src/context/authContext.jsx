9cf2ca2ec6c6dfd6e5f2e0fc1e68dc04b38cf903
GOODBADBOY10 <selfmanademola@gmail.com>
2024-12-17 09:19:09 +0100

    removed comment

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -15,12 +15,6 @@ export function useAuth() {
 export function UserAuthContextProvider({ children }) {
     const [user, setUser] = useState({});
 
-    // function logIn(email, password) {
-    //   return signInWithEmailAndPassword(auth, email, password);
-    // }
-    // function signUp(email, password) {
-    //   return createUserWithEmailAndPassword(auth, email, password);
-    // }
     function logOut() {
       return signOut(auth);
     }
